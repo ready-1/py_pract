3a244e6424c24d11d61676e998e8ddc2af53d87a
Bob King <bob@getready1.com>
2020-05-02 17:48:02 -0400

    Stub for 004_divisors.py

---
+++ src/004_divisors.py
@@ -0,0 +1,17 @@
+#!../bin/python
+
+"""004_divisors.py
+
+Python learning project from
+http://www.practicepython.org/exercise/2014/02/26/04-divisors.html
+
+Create a program that asks the user for a number and then prints out a list 
+of all the divisors of that number. (If you don’t know what a divisor is, it 
+is a number that divides evenly into another number. For example, 13 is a 
+divisor of 26 because 26 / 13 has no remainder.)
+
+"""
+
+print('Hello world! 004_divisors.py')
+
+

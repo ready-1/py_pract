8b0b3119d3044e75a91af132869aaa41f4bfdbf2
Bob King <bob@getready1.com>
2020-05-10 21:00:01 -0400

    Completed

--- src/004_divisors.py
+++ src/004_divisors.py
@@ -11,20 +11,23 @@ is a number that divides evenly into another number. For example, 13 is a
 divisor of 26 because 26 / 13 has no remainder.)
 
 """
+# This is the one I was working on when my dad passed away.  Love you Pop. 5/2/20
 
 # get a number from the user
 num = int(input("\nEnter a number: "))
 
-results = [] # the list of divisors
+results = '' # the list of divisors
 
-# loop over the range of numbers from 1 to num + 1
-# we don't want to divide by zero and we want to account
-# for num/num = 1
+# loop over the range of numbers from 1 to num - 1
 for i in range(1, num + 1):
-    #test if i is a divisor
-    if (num % i == 0):
+    # test if i is a divisor
+    if (num % i == 0 and i != num):
         # if yes, then add it to the list
-        results.append(i)
+        results += str(i) + ', '
+    elif (num % i == 0 and i == num):
+        results += str(i)
 
 
+
+# show the results
 print(f'\nYou entered {num} and it\'s dividors are {results}.\n\n')

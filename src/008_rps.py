54fc244bd9ad2f611386ca4b2eb47af9118f1ef7
Bob King <bob@getready1.com>
2020-05-12 11:59:30 -0400

    Exercise complete

--- src/008_rps.py
+++ src/008_rps.py
@@ -18,3 +18,38 @@ Remember the rules:
 """
 
 print('Hello World! This is 008_rps.py')
+
+player_1 = ''
+player_2 = ''
+
+def score_game(player_1, player_2):
+    winner = 'Player 2 wins!'
+    # rock beats scissors
+    if player_1 == 'r' and player_2 == 's':
+        winner == 'Player 1 wins!'
+    # scissors beats paper
+    if player_1 == 's' and player_2 =='p':
+        winner == 'Player 1 wins!'
+    # paper beats rock
+    if player_1 == 'p' and player_2 == 'r':
+        winner == 'Player 1 wins!'
+    # its a tie!
+    if player_1 == player_2:
+        winner = 'It\'s a tie!'
+    return winner
+
+def get_input(player):
+    #prompt player for input
+    print(' r for rock\n p for paper\n s for scissors')
+    return input(f'Player {player} - Enter your play: ')
+
+while True:
+    print('=' * 40)
+    # instructions to quit
+    print(f'<Ctrl-c> to exit\n')
+    # get user input
+    player_1 = get_input(1)
+    player_2 = get_input(2)
+    # print the result
+    print(f'\n{">" * 10} {score_game(player_1, player_2)} {"<" * 10}')
+    print(f'\nPress <Enter> for another game...')

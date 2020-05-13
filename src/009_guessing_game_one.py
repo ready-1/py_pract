f46eaf7636f36e89b331edc05b9a1a25d2b612fb
Bob King <bob@getready1.com>
2020-05-12 21:12:37 -0400

    009_guessing_gmae_one stubbed.  README updated

---
+++ src/009_guessing_game_one.py
@@ -0,0 +1,21 @@
+#!../bin/python
+
+"""009_guessing_game_one.py
+
+A Python learning project from 
+http://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html
+
+Generate a random number between 1 and 9 (including 1 and 9). Ask the user 
+to guess the number, then tell them whether they guessed too low, too high, 
+or exactly right. (Hint: remember to use the user input lessons from the 
+very first exercise)
+
+Extras:
+
+1. Keep the game going until the user types “exit”
+2. Keep track of how many guesses the user has taken, and when the game ends, 
+    print this out.
+
+"""
+
+print('Hello World!  This is 009_guessing_game_one.py')

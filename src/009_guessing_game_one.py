4fe3fdd3d67de85c500e1cc99f135daf319e27c6
Bob King <bob@getready1.com>
2020-05-13 11:36:49 -0400

    Finished.

--- src/009_guessing_game_one.py
+++ src/009_guessing_game_one.py
@@ -18,21 +18,72 @@ Extras:
 
 """
 
+# I kind of went off the rails on this one, free-coding pretty much the 
+# entire thing.  Not too much overall design, just a lot of fun.
+
 import random
 
 # define the boundaries
 upper_bound = 9
 lower_bound = 1
-# generate a random number
-rand_num = random.randint(lower_bound, upper_bound)
+game_counter = 0 # how many games have been played
+guess_counter = 0 # number of guesses for each round
+guess_state = 'correct' # if this is 'correct' gen a new random num
+rand_num = 0 # number to try to guess
 
-# get user input
-guess = int(input(f'Enter a guess between {lower_bound} and {upper_bound}: '))
 
-if (guess < rand_num):
-    print(f'Your guess ({guess}) is too low.')
-elif (guess > rand_num):
-    print(f'Your guess ({guess}) is too high.')
-else:
-    print(f'You guessed correctly!!  {guess} is correct.')
 
+play = True # this is the game state
+
+def gen_random():
+    # generate a random number
+    return random.randint(lower_bound, upper_bound)
+
+def check_guess(guess):
+    global play
+    try:
+        if (int(guess) < rand_num):
+            return 'too low'
+        elif (int(guess) > rand_num):
+            return 'too high'
+        elif (int(guess) == rand_num):
+            return 'correct'
+    
+    except:
+        if (guess == 'exit'):
+            play = False
+            return 'exit'
+        else:
+            return '-1'
+
+def new_game():
+    global rand_num
+    global game_counter
+    global guess_counter
+    rand_num = gen_random()
+    game_counter += 1
+    guess_counter = 0
+    print('\nNew Game!')
+    print(f'Enter "exit" to quit.\n')
+    return
+
+# start the first game
+new_game()
+while play:
+    loop = True
+    while (True):
+        guess = input(f'Enter a guess between {lower_bound} and {upper_bound}: ')
+        result = check_guess(guess)
+        if (result != '-1'):
+            break
+        print('<< Invalid Input - Try Again >>')
+
+    guess_counter += 1
+
+    if (result == 'correct'):
+        print(f'Tries: {guess_counter} - Your guess ({guess}) is {result}.')
+        print('You Win!')
+        print(f'Your stats: games played: {game_counter} guesses/game {guess_counter/game_counter}')
+        new_game()
+    elif (result == 'too high' or result == 'too low'):
+        print(f'Tries: {guess_counter} - Your guess ({guess}) is {result}.')

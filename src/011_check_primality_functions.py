3ef6fd96ac964da6cc190867be30e4269bceb2fd
Bob King <bob@getready1.com>
2020-05-21 19:41:12 -0400

    stub ex 011_prim...unctions.py and update README

---
+++ src/011_check_primality_functions.py
@@ -0,0 +1,14 @@
+#!../bin/python
+
+"""011_check_primality_functions.py
+
+Ask the user for a number and determine whether the number is prime or not. 
+(For those who have forgotten, a prime number is a number that has 
+no divisors.). 
+
+"""
+
+print('Hello World! This is 011_check_primality_functions.py')
+
+
+

4fb7c77160eb3028282585c069fd86f3f97228d4
Bob King <bob@getready1.com>
2020-05-22 15:50:38 -0400

    Stub ex 012_list_ends.py and update README

---
+++ src/012_list_ends.py
@@ -0,0 +1,15 @@
+#!../bin/python
+
+"""012_list_ends.py
+
+
+http://www.practicepython.org/exercise/2014/04/25/12-list-ends.html
+
+Write a program that takes a list of numbers (for example, 
+a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last 
+elements of the given list. For practice, write this code inside a function.
+
+"""
+
+print('Hello World! This is 012_list_ends.py')
+

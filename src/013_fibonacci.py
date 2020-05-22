06f9e07a6ce9d917627b6035c865eccf35281a4c
Bob King <bob@getready1.com>
2020-05-22 16:25:51 -0400

    Stub 013_fibonacci.py and update README

---
+++ src/013_fibonacci.py
@@ -0,0 +1,19 @@
+#!../bin/python
+
+"""013_fibonacci.py
+
+http://www.practicepython.org/exercise/2014/04/30/13-fibonacci.html
+
+Write a program that asks the user how many Fibonnaci numbers to generate 
+and then generates them. Take this opportunity to think about how you can 
+use functions. Make sure to ask the user to enter the number of numbers in 
+the sequence to generate.(Hint: The Fibonnaci seqence is a sequence of 
+numbers where the next number in the sequence is the sum of the previous 
+two numbers in the sequence. 
+
+The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+
+"""
+
+print('Hello World! This is 013_fibonacci.py\n')
+

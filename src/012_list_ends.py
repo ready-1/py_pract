26c3577dc866493171b6093ad7d53a8dffee4776
Bob King <bob@getready1.com>
2020-05-22 16:09:30 -0400

    Using indexes

--- src/012_list_ends.py
+++ src/012_list_ends.py
@@ -11,5 +11,25 @@ elements of the given list. For practice, write this code inside a function.
 
 """
 
-print('Hello World! This is 012_list_ends.py')
+print('Hello World! This is 012_list_ends.py\n')
+
+# our list of values
+list_a = [5, 10, 15, 20, 25]
+print(f'Our original list is {list_a}')
+# a new list containing the first and last items from list_a
+list_b = []
+
+
+
+# using index values
+# get the ffirst item
+list_b.append(list_a[0])
+#get the last item
+list_b.append(list_a[-1])
+# display the results
+print(f'Using index values, the first and last values are {list_b}')
+
+print('Done.')
+
+
 

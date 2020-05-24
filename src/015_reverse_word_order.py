54960e94e8339e0b409921c7f44081cb4d587639
Bob King <bob@getready1.com>
2020-05-24 14:44:54 -0400

    Add 015_reverse_word_order. Update README

---
+++ src/015_reverse_word_order.py
@@ -0,0 +1,24 @@
+#!../bin/python
+
+"""015_reverse_word_order.py
+
+This is a Python learning project from:
+http://www.practicepython.org/exercise/2014/05/21/15-reverse-word-order.html
+
+Write a program (using functions!) that asks the user for a long string 
+containing multiple words. Print back to the user the same string, except 
+with the words in backwards order. For example, say I type the string:
+
+  My name is Michele
+
+Then I would see the string:
+
+  Michele is name My
+
+shown back to me.
+
+"""
+
+print('Hello World! This is 015_reverse_word_order.py\n')
+
+

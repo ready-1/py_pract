f4ec98d540c375d58dce3692e56f489dd4d0bffd
Bob King <bob@getready1.com>
2020-05-23 21:13:06 -0400

    Add 014_list_remove_dups. Update README

---
+++ src/014_list_remove_duplicates.py
@@ -0,0 +1,24 @@
+#!../bin/python
+
+"""014_list_remove_duplicates.py
+
+This is a Python learning project from:
+http://www.practicepython.org/exercise/2014/05/15/14-list-remove-duplicates.html
+
+Write a program (function!) that takes a list and returns a new list that 
+contains all the elements of the first list minus all the duplicates.
+
+Extras:
+
+    1. Write two different functions to do this - one using a loop and 
+    constructing a list, and another using sets.
+
+    2. Go back and do Exercise 5 using sets, and write the solution for 
+    that in a different function.
+
+
+"""
+
+print('Hello World! This is 014_list_remove_duplicates.py\n')
+
+

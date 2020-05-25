4c11ac0b1fc6bb8e3d861428d9a66bd8ca3e6e2d
Bob King <bob@getready1.com>
2020-05-25 18:49:35 -0400

    Add 017_decod..ge.py, BS4 & requests, updt README

---
+++ src/017_decode_web_page.py
@@ -0,0 +1,20 @@
+#!../bin/python
+
+"""017_decode_web_page.py
+
+This is a Python learning project from:
+http://www.practicepython.org/exercise/2014/06/06/17-decode-a-web-page.html
+
+Use the BeautifulSoup and requests Python packages to print out a list of 
+all the article titles on the New York Times homepage.
+
+"""
+
+
+
+print('Hello World! This is 017_decode_web_page.py\n')
+
+
+
+
+
